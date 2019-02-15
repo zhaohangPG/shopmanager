3f63cab942dd154a6fa9895a5a6a05deced03b78
zhaohangPG <443658122@qq.com>
2019-02-15 09:09:31 +0800

    'home结构布局'

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,7 +1,15 @@
 <template>
-    <el-container>
-        <el-header class="header">Header</el-header>
-        <el-container class="container">
+    <el-container class="container">
+        <el-header class="header">
+            <el-row>
+                <el-col :span="2"><div class="grid-content bg-purple">1</div></el-col>
+                <el-col :span="20"><div class="grid-content bg-purple-light">2</div></el-col>
+                <el-col :span="2"><div class="grid-content bg-purple">
+                    <a href="" class="loginout">退出</a>
+                    </div></el-col>
+            </el-row>
+        </el-header>
+        <el-container >
             <el-aside width="200px" class="aside">Aside</el-aside>
             <el-main>Main</el-main>
         </el-container>
@@ -17,5 +25,18 @@ export default {
 <style>
 .header {
     background-color: #ff0000;
+}
+.aside {
+    background: #0f0;
+    /* height: 100%; */
+}
+.container {
+    height: 100%;
+}
+.loginout {
+    text-decoration: none;
+    color: #000;
+    text-align: center;
+    line-height: 60px;
 }
 </style>
