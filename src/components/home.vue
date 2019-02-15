e37740e586bb253b94ea3e1bee72ff09386604b8
zhaohangPG <443658122@qq.com>
2019-02-15 10:39:53 +0800

    'home界面组件基本搭建'

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,33 +2,52 @@
     <el-container class="container">
         <el-header class="header">
             <el-row>
-                <el-col :span="2"><div class="grid-content bg-purple">1</div></el-col>
-                <el-col :span="20"><div class="grid-content bg-purple-light">2</div></el-col>
                 <el-col :span="2"><div class="grid-content bg-purple">
-                    <a href="" class="loginout">退出</a>
+                    <img src="../assets/logo.png" alt=""></div></el-col>
+                <el-col :span="20"><div class="grid-content bg-purple-light tac">
+                    <h2 class="title">电商后台管理页面</h2>
+                    </div></el-col>
+                <el-col :span="2"><div class="grid-content bg-purple tac pt20">
+                     <el-button type="danger">退出</el-button>
+                    <!-- <a href="" class="loginout" @click.prevent="logout()">退出</a> -->
                     </div></el-col>
             </el-row>
         </el-header>
         <el-container >
-            <el-aside width="200px" class="aside">Aside</el-aside>
+            <el-aside width="200px" class="aside">
+                <app-menu></app-menu>
+            </el-aside>
             <el-main>Main</el-main>
         </el-container>
     </el-container>
 </template>
 
 <script>
+import appMenu from './menu'
 export default {
+  data () {
+    return {
 
+    }
+  },
+  methods: {
+    logout () {
+
+    }
+  },
+  components: {
+    appMenu
+  },
+  name: 'app'
 }
 </script>
 
 <style>
 .header {
-    background-color: #ff0000;
+    background-color: gray;
 }
 .aside {
-    background: #0f0;
-    /* height: 100%; */
+    background-color: #fff;
 }
 .container {
     height: 100%;
@@ -38,5 +57,15 @@ export default {
     color: #000;
     text-align: center;
     line-height: 60px;
+}
+.tac {
+    text-align: center;
+}
+.title {
+    line-height: 60px;
+    color: #fff;
+}
+.pt20 {
+    padding-top: 10px;
 }
 </style>
