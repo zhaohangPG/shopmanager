c7c349c74eb1fac9a1f7630de568f2094da6f14f
zhaohangPG <443658122@qq.com>
2019-02-18 22:54:13 +0800

    '动态菜单完毕'

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,57 +1,44 @@
 <template>
-  <el-menu :unique-opened='true' default-active="2" :router='true' class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
-    <el-submenu index="1">
+  <el-menu :unique-opened="true" default-active="2" :router="true" class="el-menu-vertical-demo">
+    <el-submenu v-for="item in menuData" :key="item.id" :index="item.order+''">
       <template slot="title">
         <i class="el-icon-location"></i>
-        <span>用户管理</span>
+        <span>{{item.authName}}</span>
       </template>
-        <el-menu-item index="users"><i class="el-icon-tickets"></i>用户列表</el-menu-item>
-    </el-submenu>
-    <el-submenu index="2">
-      <template slot="title">
-        <i class="el-icon-location"></i>
-        <span>权限管理</span>
-      </template>
-        <el-menu-item index="roles"><i class="el-icon-tickets"></i>角色列表</el-menu-item>
-        <el-menu-item index="rights"><i class="el-icon-tickets"></i>权限列表</el-menu-item>
-    </el-submenu>
-    <el-submenu index="3">
-      <template slot="title">
-        <i class="el-icon-location"></i>
-        <span>商品管理</span>
-      </template>
-        <el-menu-item index="users"><i class="el-icon-tickets"></i>商品列表</el-menu-item>
-        <el-menu-item index="users"><i class="el-icon-tickets"></i>分类参数</el-menu-item>
-        <el-menu-item index="users"><i class="el-icon-tickets"></i>商品分类</el-menu-item>
-    </el-submenu>
-    <el-submenu index="4">
-      <template slot="title">
-        <i class="el-icon-location"></i>
-        <span>订单管理</span>
-      </template>
-        <el-menu-item index="users"><i class="el-icon-tickets"></i>订单列表</el-menu-item>
-    </el-submenu>
-    <el-submenu index="4">
-      <template slot="title">
-        <i class="el-icon-location"></i>
-        <span>数据统计</span>
-      </template>
-        <el-menu-item index="users"><i class="el-icon-tickets"></i>数据报表</el-menu-item>
+      <el-menu-item v-for="subitem in item.children" :key="subitem.id" :index="subitem.path+''">
+        <i class="el-icon-tickets"></i>{{subitem.authName}}
+      </el-menu-item>
     </el-submenu>
   </el-menu>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      menuData:[]
+    };
+  },
+  created() {
+    this.getMenuData();
+  },
   methods: {
-    handleOpen (key, keyPath) {
-      console.log(key, keyPath)
-    },
-    handleClose (key, keyPath) {
-      console.log(key, keyPath)
+    getMenuData() {
+      this.$http.get("menus").then(res => {
+        const {
+          data: {
+            data,
+            meta: { status }
+          }
+        } = res;
+        if (status === 200) {
+          console.log(data);
+          this.menuData = data;
+        }
+      });
     }
   }
-}
+};
 </script>
 
 <style>
