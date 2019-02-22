b628621e23dd69b7c21c8222af22a854040de021
zhaohangPG <443658122@qq.com>
2019-02-22 08:55:29 +0800

    '报表完成'

--- src/components/reports.vue
+++ src/components/reports.vue
@@ -1,9 +1,7 @@
 <template>
   <el-card class="box">
     <cru-link level1="数据统计" level2="数据报表"></cru-link>
-    <div class="echarts" id="main">
-
-    </div>
+    <div class="echarts" id="main"></div>
   </el-card>
 </template>
 
@@ -17,92 +15,43 @@ export default {
   mounted() {
     this.$http.get("reports/type/1").then(res => {
       console.log(res);
-    });
-    var myChart = echarts.init(document.getElementById('main'));
-    const option = {
-      title: {
-        text: "堆叠区域图"
-      },
-      tooltip: {
-        trigger: "axis",
-        axisPointer: {
-          type: "cross",
-          label: {
-            backgroundColor: "#6a7985"
-          }
+      const {
+        data: {
+          data,
+          meta: { msg, status }
         }
-      },
-      legend: {
-        data: ["邮件营销", "联盟广告", "视频广告", "直接访问", "搜索引擎"]
-      },
-      toolbox: {
-        feature: {
-          saveAsImage: {}
-        }
-      },
-      grid: {
-        left: "3%",
-        right: "4%",
-        bottom: "3%",
-        containLabel: true
-      },
-      xAxis: [
-        {
-          type: "category",
-          boundaryGap: false,
-          data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"]
-        }
-      ],
-      yAxis: [
-        {
-          type: "value"
-        }
-      ],
-      series: [
-        {
-          name: "邮件营销",
-          type: "line",
-          stack: "总量",
-          areaStyle: {},
-          data: [120, 132, 101, 134, 90, 230, 210]
+      } = res;
+      const option1 = data;
+
+      var myChart = echarts.init(document.getElementById("main"));
+      const option2 = {
+        title: {
+          text: "堆叠区域图"
         },
-        {
-          name: "联盟广告",
-          type: "line",
-          stack: "总量",
-          areaStyle: {},
-          data: [220, 182, 191, 234, 290, 330, 310]
-        },
-        {
-          name: "视频广告",
-          type: "line",
-          stack: "总量",
-          areaStyle: {},
-          data: [150, 232, 201, 154, 190, 330, 410]
-        },
-        {
-          name: "直接访问",
-          type: "line",
-          stack: "总量",
-          areaStyle: { normal: {} },
-          data: [320, 332, 301, 334, 390, 330, 320]
-        },
-        {
-          name: "搜索引擎",
-          type: "line",
-          stack: "总量",
-          label: {
-            normal: {
-              show: true,
-              position: "top"
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "cross",
+            label: {
+              backgroundColor: "#6a7985"
             }
-          },
-          areaStyle: { normal: {} },
-          data: [820, 932, 901, 934, 1290, 1330, 1320]
+          }
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        grid: {
+          left: "3%",
+          right: "4%",
+          bottom: "3%",
+          containLabel: true
         }
-      ]
-    };
-    myChart.setOption(option);
+      };
+      const option = { ...option1, ...option2 };
+      myChart.setOption(option);
+    });
   }
 };
 </script>
@@ -112,7 +61,7 @@ export default {
   height: 100%;
 }
 .echarts {
-    width: 600px;
-    height: 400px;
+  width: 600px;
+  height: 400px;
 }
 </style>
